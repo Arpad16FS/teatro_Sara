def6b9766d5ef8567fe8d71205c3f0f4ae619fdf
Arpad16FS <arpd.612@gmail.com>
2020-08-27 18:34:39 -0500

    Added some CSS

--- recursos/js/basicos.js
+++ recursos/js/basicos.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     $('.nav-btn').click(function() {
-        /*$(this).parent().find('.fa').addClass('bars-anim').on('animationend', function() {
-            $(this).toggleClass('fa-bars').toggleClass('fa-ellipsis-v').removeClass('bars-anim');
-        });*/
-        $(this).parent().find('.fa-bars').addClass('bars-anim').on('animationend', function() {
-            $(this).removeClass('fa-bars').addClass('fa-ellipsis-v').removeClass('bars-anim');
-        });
-        $(this).parent().find('.fa-ellipsis-v').addClass('dots-anim').on('animationend', function() {
-            $(this).addClass('fa-bars').removeClass('fa-ellipsis-v').removeClass('dots-anim');
-        });
         $(this).parent().find('.hidden-nav').toggle();
         console.log('btn');
+    });
+
+    /*title scroll effect*/
+    let titleText = $('.title-container__text');
+    let titleBG = $('.title-container');
+    $(window).scroll(function() {
+        var scrolled = window.scrollY;
+        var val2 = 150-scrolled;
+        titleBG.css({
+            'background-image': 'linear-gradient(rgb(' +val2+ ',' +scrolled+ ',' +scrolled+ '), rgb(' +val2+ ',' +val2+ ',' +scrolled+ '))'
+        });
+        console.log(val2 + 'px');
     });
 });
--- vistas/pagMaestra/inicio.php
+++ vistas/pagMaestra/inicio.php
@@ -2,20 +2,20 @@
     require 'vistas/diseños/cabecera.php';
 ?>
 <body>
-    <div class="wrapper fb-sbtwn">
-        <div class="title-container fb-sbtwn">
-            <h1>Gran Teatro Sara</h1>
+    <div class="wrapper fb-s-btwn">
+        <div class="title-container">
+            <h1 class="title-container__text">Gran Teatro Sara</h1>
         </div>
-        <div class="text-container fb-sbtwn">
+        <div class="text-container fb-s-btwn">
             <h2>Bienvenido a la nueva forma de disfrutar el teatro</h2>
             <p>En hora buena, estas a punto de experimentar una experiencia sin igual, ya casi estás ahí, solo debes <b>llegar</b> montandote al bus.</p>
         </div>
-        <div class="nav-1-container fb-sbtwn">
+        <div class="nav-1-container fb-s-btwn">
             <a class="a-bus f-50p">Llegar</a>
         </div>
-        <div class="nav-2-container fb-sbtwn">
+        <div class="nav-2-container fb-s-btwn">
             <a class="f-50p fb-center nav-btn"><i class="fa fa-bars" style="flex: 0 0;"></i></a>
-            <div class="hidden-nav fb-sbtwn">
+            <div class="hidden-nav fb-s-btwn">
                 <a class="a-bus f-50p">Nuestro proyecto</a>
                 <a class="a-bus f-50p">Contactanos</a>
                 <a class="a-bus f-50p">¿Quiénes somos?</a>
