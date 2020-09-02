f6122422565232c198ab312b147bc4e2808448e6
Arpad16FS <arpd.612@gmail.com>
2020-09-02 15:29:56 -0500

    Optimized CSS and JS

--- recursos/js/basicos.js
+++ recursos/js/basicos.js
@@ -1,39 +1,59 @@
-$(document).ready(function(){
-    $('.nav-btn').click(function() {
-        $(this).parent().find('.hidden-nav').toggle();
-        console.log('btn');
-    });
-
+$(document).ready(function () {
     /*title scroll effect*/
-    let titleText = $('.title-container__text');
-    let titleBG = $('.title-container');
-    let titleMoon = $('.title-container__img--moon');
-    let titleSun = $('.title-container__img--sun');
-    $(window).scroll(function() {
-        var scrolled = window.scrollY;
-        var scrolledPrcnt = window.scrollY/titleBG.height()*100;
-        var val1 = Math.round(Math.min(255, scrolled/titleBG.height()*255));
-        var val2 = Math.round(Math.max(0, 150-scrolled/titleBG.height()*150));
-        var val3 = 20-scrolledPrcnt*0.4;
-        titleBG.css({
-            'background-image': 'linear-gradient(rgb(' +val2+ ',' +val1+ ',' +val1+ '), rgb(' +val2+ ',' +val2+ ',' +val1+ '))'
-        });
-        titleText.css({
-            'top': scrolled*0.5 + 'px'
-        });
-        
-        titleMoon.css({
-            'top': 50+scrolledPrcnt*0.5 + '%',
-            // 'top': titleMoon.height()+scrolled/titleBG.height()*(titleBG.height()-titleMoon.height()) + 'px',
-            'left': 100-scrolledPrcnt + '%',
-            'transform': 'translateY(-50%)'+'rotate('+ val3 +'deg)'
-        });
-        titleSun.css({
-            'top': 50+scrolledPrcnt*0.5 + '%',
-            'left': 50-scrolledPrcnt + '%',
-            'transform': 'translate(-50%, -50%)'+'rotate('+ '-' + scrolledPrcnt*0.3 +'deg)'
-        });
+    var titleText = $('.title-container__text');
+    var titleBG = $('.title-container');
+    var titleMoon = $('.title-container__img--moon');
+    var titleSun = $('.title-container__img--sun');
+    var vpHeight = $(window).height();
+    // Detect request animation frame
+    var scroll = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        // IE Fallback, you can even fallback to onscroll
+        function (callback) { window.setTimeout(callback, 1000 / 60) };
 
-        console.log(val2 + 'px');
-    });
+    var lastPosition = -1;
+    function loop() {
+        // Avoid calculations if not needed
+        if (lastPosition == window.pageYOffset) {
+            scroll(loop);
+            return false;
+        } else lastPosition = window.pageYOffset;
+        // do the onscroll stuff you want here
+        var scrolled = window.pageYOffset;
+        let scrolledPrcnt = Math.round(window.pageYOffset / vpHeight * 50);
+        let sunXOffset = -50-scrolledPrcnt;
+        let sunXOffset2 = 50-scrolledPrcnt;
+        let imgYOffset = -50-(scrolledPrcnt*0.5);
+        let imgYOffset2 = 50+(scrolledPrcnt*2);
+        let moonXOffset = Math.max(0, 100-scrolledPrcnt);
+        let moonRotation = Math.round(30 - Math.min(60, scrolledPrcnt*0.6));
+        let sunRotation = Math.round(-Math.min(30, scrolledPrcnt*0.6));
+        //a let val1 = Math.round(Math.min(255, scrolled / titleBG.height() * 255));
+        //a let val2 = Math.round(Math.max(0, 150 - scrolled / titleBG.height() * 150));
+        //a titleBG.css({
+        //a     'background-image': 'linear-gradient(rgb(' + val2 + ',' + val1 + ',' + val1 + '), rgb(' + val2 + ',' + val2 + ',' + val1 + '))'
+        //a });
+        //a titleText.css({
+        //a     'top': Math.round(scrolled * 0.5) + 'px'
+        //a });
+
+        titleSun.css({
+            'transform': 'translate3d(calc('+sunXOffset+'% + ' +sunXOffset2+ 'vw),' + 'calc('+imgYOffset+'% + ' +imgYOffset2+ 'vh),0)'+
+            'rotate('+sunRotation+'deg)'
+        });
+        titleMoon.css({
+            'transform': 'translate3d(calc(-'+scrolledPrcnt+'% + ' +moonXOffset+ 'vw),' + 'calc('+imgYOffset+'% + ' +imgYOffset2+ 'vh),0)'+
+            'rotate('+moonRotation+'deg)'
+        });
+        console.log(scrolledPrcnt + '%');
+        // Recall the loop
+        scroll(loop)
+    }
+
+    // Call the loop for the first time
+    loop();
+    /*ETSFx*/
 });
--- vistas/pagMaestra/inicio.php
+++ vistas/pagMaestra/inicio.php
@@ -1,6 +1,7 @@
 <?php
-    require 'vistas/diseños/cabecera.php';
+require 'vistas/diseños/cabecera.php';
 ?>
+
 <body>
     <div class="wrapper fb-s-btwn">
         <div class="title-container">
@@ -8,25 +9,28 @@
             <img class="title-container__img title-container__img--moon" src="recursos/imagenes/pagMaestra/moon-in-his-splendor-1.svg" alt="La Luna">
             <h1 class="title-container__text">Gran Teatro Sara</h1>
         </div>
-        <div class="text-container fb-s-btwn">
-            <h2>Bienvenido a la nueva forma de disfrutar el teatro</h2>
-            <p>En hora buena, estas a punto de experimentar una experiencia sin igual, ya casi estás ahí, solo debes <b>llegar</b> montandote al bus.</p>
+    </div>
+    <div class="text-container fb-s-btwn">
+        <h2>Bienvenido a la nueva forma de disfrutar el teatro</h2>
+        <p>En hora buena, estas a punto de experimentar una experiencia sin igual, ya casi estás ahí, solo debes <b>llegar</b> montandote al bus.</p>
+    </div>
+    <div class="nav-1-container fb-s-btwn">
+        <a class="a-bus f-50p">Llegar</a>
+    </div>
+    <div class="nav-2-container fb-s-btwn">
+        <a class="f-50p fb-center nav-btn"><i class="fa fa-bars" style="flex: 0 0;"></i></a>
+        <div class="hidden-nav fb-s-btwn">
+            <a class="a-bus f-50p">Nuestro proyecto</a>
+            <a class="a-bus f-50p">Contactanos</a>
+            <a class="a-bus f-50p">¿Quiénes somos?</a>
         </div>
-        <div class="nav-1-container fb-s-btwn">
-            <a class="a-bus f-50p">Llegar</a>
-        </div>
-        <div class="nav-2-container fb-s-btwn">
-            <a class="f-50p fb-center nav-btn"><i class="fa fa-bars" style="flex: 0 0;"></i></a>
-            <div class="hidden-nav fb-s-btwn">
-                <a class="a-bus f-50p">Nuestro proyecto</a>
-                <a class="a-bus f-50p">Contactanos</a>
-                <a class="a-bus f-50p">¿Quiénes somos?</a>
-            </div>
-        </div>
+    </div>
     </div>
     <?php echo $this->mensaje; ?>
     <?php
-        require 'vistas/diseños/piePag.php';
+    require 'vistas/diseños/piePag.php';
     ?>
+    <!-- <script src="recursos/js/scroll.js"></script> -->
 </body>
+
 </html>
